3cec0f85cafbcdbfd653d3442aff5f3172fc6980
polyukhovich-olexandr <leanl3012@gmail.com>
2024-02-19 17:14:23 +0200

    added controler class

--- pycalc.py
+++ pycalc.py
@@ -5,7 +5,7 @@
 # pycalc.py
 
 import sys
-
+from functools import partial
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QApplication,
@@ -28,6 +28,7 @@ def main():
     pycalcApp = QApplication([])
     pycalcWindow = PyCalcWindow()
     pycalcWindow.show()
+    PyCalc(model=evaluateExpression, view=pycalcWindow)
     sys.exit(pycalcApp.exec())
 
 
@@ -83,13 +84,41 @@ class PyCalcWindow(QMainWindow):
         self.display.setReadOnly(True)
         self.generalLayout.addWidget(self.display)
 
-    def evaluateExpression(expression):
-        """Evaluate an expression (Model)."""
-        try:
-            result = str(eval(expression, {}, {}))
-        except Exception:
-            result = ERROR_MSG
-        return result
+
+def evaluateExpression(expression):
+    """Evaluate an expression (Model)."""
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERROR_MSG
+    return result
+
+
+class PyCalc:
+    """PyCalc's controller class."""
+
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connectSignalsAndSlots()
+
+    def _calculateResult(self):
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, subExpression):
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+        expression = self._view.displayText() + subExpression
+        self._view.setDisplayText(expression)
+
+    def _connectSignalsAndSlots(self):
+        for keySymbol, button in self._view.buttonMap.items():
+            if keySymbol not in {"=", "C"}:
+                button.clicked.connect(partial(self._buildExpression, keySymbol))
+        self._view.buttonMap["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
 
 
 if __name__ == "__main__":
